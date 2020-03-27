a92ef9ec7b61f89eee37c76b16d6b9c45c1b5e71
Nadine Groot-Bernink <nadine.groot-bernink@mediabeam.com>
2020-03-27 15:28:11 +0100

    Liste wird in App ausgegeben

--- app/src/main/java/com/example/listviewdemo/MainActivity.java
+++ app/src/main/java/com/example/listviewdemo/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.listviewdemo;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView) findViewById(R.id.myListView);
+
+        final ArrayList<String> myFamily = new ArrayList<String>();
+        myFamily.add("Nadine");
+        myFamily.add("Birgit");
+        myFamily.add("Josef");
+        myFamily.add("Hildegard");
+        myFamily.add("Hedwig");
+        myFamily.add("Bernhard");
+        myFamily.add("Ulla");
+        myFamily.add("Yoshi");
+
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myFamily);
+
+        listView.setAdapter(arrayAdapter);
+
+
     }
 }
